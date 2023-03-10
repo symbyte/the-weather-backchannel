a62c75adb2bcaef9b05fb4691e959e5cdd68ada4
Steven Chambers <stevenchambers.exe@gmail.com>
2023-03-09 18:45:44 -0600

    finish fleshing out Forecast models

--- src/main/scala/symbyte/theweatherbackchannel/domain/models/Forecast.scala
+++ src/main/scala/symbyte/theweatherbackchannel/domain/models/Forecast.scala
@@ -1,5 +1,6 @@
 package symbyte.theweatherbackchannel.domain.models
 
+import cats.syntax.option._
 import enumeratum._
 
 sealed trait TempFeels extends EnumEntry.Lowercase
@@ -9,6 +10,25 @@ object TempFeels extends Enum[TempFeels] with CirceEnum[TempFeels] {
   case object Warm extends TempFeels
   case object Cool extends TempFeels
   case object Cold extends TempFeels
+
+  // doing a simpler pure integer conversion here, no need to be
+  // as precise due to the mapping done below
+  private def celsiusToFahrenheit(t: Int): Int = (t * 2) + 32
+
+  def fromTemperature(temp: Int, unit: String): Option[TempFeels] = {
+    val tempInFahrenheit: Option[Int] = unit.toLowerCase match {
+      case "f" => temp.some
+      case "c" => celsiusToFahrenheit(temp).some
+      case _   => none
+    }
+
+    tempInFahrenheit map {
+      case t if t > 90 => TempFeels.Hot
+      case t if t > 70 => TempFeels.Warm
+      case t if t > 40 => TempFeels.Cool
+      case _           => TempFeels.Cold
+    }
+  }
 }
 
 case class Forecast(conditions: String, temperature: TempFeels)
